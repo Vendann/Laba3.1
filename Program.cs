0c0bbac3c7bf563b041760d5552ac3016a504c76
Vendann <tigrun324@gmail.com>
2022-05-31 20:34:57 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassSegment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+            p1.Show();
+            Point p2 = new Point(12, 13);
+            p2.Show();
+            Segment r = new Segment(p1, p2);
+            r.Show();
+            double d = r.Length();
+            Console.WriteLine("Длина отрезка: {0}", d);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Segment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassSegment
+{
+    class Segment
+    {
+        private Point pStart;
+        private Point pEnd;
+
+        public Segment(Point pStart, Point pEnd)
+        {
+            this.pStart = pStart;
+            this.pEnd = pEnd;
+        }
+        public Segment()
+        {
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Отрезок с координатами: {0}-{1}", pStart, pEnd);
+        }
+
+        public double Length()
+        {
+            return pStart.Interval(pEnd);
+        }
+    }
+}
